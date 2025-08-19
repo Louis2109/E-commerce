c65590d4490f98756cfb16c82a8b1eec2fa2c57f
Louis2109 <nkenfacklandoloic@gmail.com>
2025-08-19 13:57:52 +0100

    init all

--- front/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-surface-light dark:bg-surface-dark border-t border-border-light dark:border-border-dark py-6 text-center text-text-secondary-light dark:text-text-secondary-dark text-body-small">
-      © {new Date().getFullYear()} Mervason Marketplace. Tous droits réservés.
-    </footer>
-  );
-}
